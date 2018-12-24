b2823afbe6b2df9aa35394325cc1646da26eadd8
Brett Hartman <brett.hartman@mac.com>
2018-12-24 06:48:47 -0800

    Complete implementation of Mkdir command with all helper methods

--- src/files/Directory.scala
+++ src/files/Directory.scala
@@ -3,17 +3,29 @@ package files
 class Directory(override val parentPath: String, override val name: String, val contents: List[DirEntry])
   extends DirEntry(parentPath, name) {
 
-  def hasEntry(name: String): Boolean = ???
+  def hasEntry(name: String): Boolean = findEntry(name) != null
 
-  def getAllFoldersInPath: List[String] = path.substring(1).split(Directory.SEPARATOR).toList
+  def getAllFoldersInPath: List[String] =
+    path.substring(1).split(Directory.SEPARATOR).toList.filter(x => !x.isEmpty)
 
-  def findDescendant(path: List[String]): Directory = ???
+  def findDescendant(path: List[String]): Directory =
+    if (path.isEmpty) this
+    else findEntry(path.head).asDirectory.findDescendant(path.tail)
 
-  def addEntry(newEntry: DirEntry): Directory = ???
+  def addEntry(newEntry: DirEntry): Directory = new Directory(parentPath, name, contents :+ newEntry)
 
-  def findEntry(entryName: String): DirEntry = ???
+  def findEntry(entryName: String): DirEntry = {
+    @scala.annotation.tailrec
+    def findEntryHelper(name: String, contentList: List[DirEntry]): DirEntry =
+      if (contentList.isEmpty) null
+      else if (contentList.head.name.equals(name)) contentList.head
+      else findEntryHelper(name, contentList.tail)
 
-  def replaceEntry(entryName: String, newEntry: DirEntry): Directory = ???
+    findEntryHelper(entryName, contents)
+  }
+
+  def replaceEntry(entryName: String, newEntry: DirEntry): Directory =
+    new Directory(parentPath, name, contents.filter(e => !e.name.equals(entryName)) :+ newEntry)
 
   def asDirectory: Directory = this
 }
