214b9716f1511f1eb2569ecf9c588c5f816ac443
Brett Hartman <brett.hartman@mac.com>
2018-12-26 07:53:35 -0800

    Refactor Command object to make use of pattern matching

--- src/commands/Command.scala
+++ src/commands/Command.scala
@@ -24,29 +24,26 @@ object Command {
     val tokens: Array[String] = input.split(" ")
 
     if (input.isEmpty || tokens.isEmpty) emptyCommand
-    else if (MKDIR.equals(tokens(0))) {
-      if (tokens.length < 2) incompleteCommand(MKDIR)
-      else new Mkdir(tokens(1))
+    else tokens(0) match {
+      case MKDIR =>
+        if (tokens.length < 2) incompleteCommand(MKDIR)
+        else new Mkdir(tokens(1))
+      case LS => new Ls
+      case PWD => new Pwd
+      case TOUCH =>
+        if (tokens.length < 2) incompleteCommand(TOUCH)
+        else new Touch(tokens(1))
+      case CD =>
+        if (tokens.length < 2) incompleteCommand(CD)
+        else new Cd(tokens(1))
+      case RM =>
+        if (tokens.length < 2) incompleteCommand(RM)
+        else new Rm(tokens(1))
+      case ECHO => new Echo(tokens.tail)
+      case CAT =>
+        if (tokens.length < 2) incompleteCommand(CAT)
+        else new Cat(tokens(1))
+      case _ => new UnknownCommand
     }
-    else if (LS.equals(tokens(0))) new Ls
-    else if (PWD.equals(tokens(0))) new Pwd
-    else if (TOUCH.equals(tokens(0))) {
-      if (tokens.length < 2) incompleteCommand(TOUCH)
-      else new Touch(tokens(1))
-    }
-    else if (CD.equals(tokens(0))) {
-      if (tokens.length < 2) incompleteCommand(CD)
-      else new Cd(tokens(1))
-    }
-    else if (RM.equals(tokens(0))) {
-      if (tokens.length < 2) incompleteCommand(RM)
-      else new Rm(tokens(1))
-    }
-    else if (ECHO.equals(tokens(0))) new Echo(tokens.tail)
-    else if (CAT.equals(tokens(0))) {
-      if (tokens.length < 2) incompleteCommand(CAT)
-      else new Cat(tokens(1))
-    }
-    else new UnknownCommand
   }
 }
